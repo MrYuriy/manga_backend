22ee1699c69a5b28a9348ac855a617a09d44c6aa
MrYuriy <yuriybabiyk@gmail.com>
2024-03-27 20:51:00 +0100

    Add hiperlink url in capter list

--- manga_manager/serializers.py
+++ manga_manager/serializers.py
@@ -1,12 +1,19 @@
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
+from django.urls import reverse
 
 from manga_manager.models import Chapter
 
 class ChapterListSerializer(serializers.ModelSerializer):
+    detail_url = serializers.SerializerMethodField()
+
     class Meta:
         model = Chapter
-        fields = ("id", "chapter_name")
+        fields = ("id", "chapter_name", "detail_url")
+
+    def get_detail_url(self, obj):
+        request = self.context.get("request")
+        return request.build_absolute_uri(reverse("manga_menager:chapter-detail", kwargs={"pk": obj.pk}))
 
 class ChapterDetailSerializer(serializers.ModelSerializer):
     class Meta:
